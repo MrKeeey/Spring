7193250e39247c901701c7ee3f7a244fc63bedae
Pavel P <markee109582@yandex.ru>
2024-07-30 15:00:59 +0300

    Dependency Injection with field

--- secondApp/src/main/java/com/example/demo/rest/MyController.java
+++ secondApp/src/main/java/com/example/demo/rest/MyController.java
@@ -8,18 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+//    зависимость через поле, устаревший вариант
+    @Autowired
     private Teacher teacher;
 
+//      зависимость через конструктор
 //    @Autowired
 //    public MyController(Teacher teacher) {
 //        this.teacher = teacher;
 //    }
 
-    //зависимость через сеттер или любой метод, а не конструктор
-    @Autowired
-    public void testMethod(Teacher teacher) {
-        this.teacher = teacher;
-    }
+//      зависимость метод, а не конструктор
+//    @Autowired
+//    public void testMethod(Teacher teacher) {
+//        this.teacher = teacher;
+//    }
+//      зависимость через сеттер, а не конструктор
+//    @Autowired
+//    public void setTeacher(Teacher teacher) {
+//        this.teacher = teacher;
+//    }
 
     @GetMapping("/homework")
     public String getHomework() {
