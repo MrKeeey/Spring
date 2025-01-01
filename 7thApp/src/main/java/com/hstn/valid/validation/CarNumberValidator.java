14468641b19a62e3e1615a09ba6330ff3666a131
Pavel P <markee109582@yandex.ru>
2025-01-01 13:04:20 +0300

    fix upperCase error

--- 7thApp/src/main/java/com/hstn/valid/validation/CarNumberValidator.java
+++ 7thApp/src/main/java/com/hstn/valid/validation/CarNumberValidator.java
@@ -15,6 +15,7 @@ public class CarNumberValidator implements ConstraintValidator<CarNumber, String
 
     @Override
     public boolean isValid(String carNumber, ConstraintValidatorContext constraintValidatorContext) {
+        if (carNumber == null) return false;
         carNumber = carNumber.toUpperCase();
         boolean result = carNumber.endsWith(countryCode);
         return result;
--- ReadMe.txt
+++ ReadMe.txt
@@ -97,4 +97,5 @@ package 7thApp - project : continue to learn spring MVC. Validation
 - add new custom validation CarNumber
 - add new class CarNumberValidator for custom validation
 - add new filed carNumber for test our custom validation
--
+- fix upperCase error if field is empty
+
